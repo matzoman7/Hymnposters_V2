19f7bd96267ff041d97b2669ef495d33da8cbffc
matzoman7 <alexauld118@gmail.com>
2026-04-06 00:20:14 -0400

    Added Debugs

--- Assets/Scripts/VotingUI.cs
+++ Assets/Scripts/VotingUI.cs
@@ -66,7 +66,7 @@ public class VotingUI : MonoBehaviour
     {
         this.gameObject.SetActive(true);
         hasVoted = false;
-
+        Debug.Log("Voting Round Start");
         foreach (Button button in buttonList)
         {
             button.interactable = true;
